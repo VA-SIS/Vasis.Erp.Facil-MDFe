20c64294e05144eb546165b8f141a4b7a386288b
Donisetti Ferreira Cosma <donisetti@msn.com>
2025-04-22 20:45:10 -0400

    Pausa para revisao

--- Backend/Vasis.Erp.Facil.Data/Repositories/Interfaces/Base/IBaseRepository.cs
+++ Backend/Vasis.Erp.Facil.Data/Repositories/Interfaces/Base/IBaseRepository.cs
@@ -1,20 +1,15 @@
-﻿using System.Linq.Expressions;
-using Vasis.Erp.Facil.Application.Dtos.Shared;
+﻿using Vasis.Erp.Facil.Application.Dtos.Shared;
+using Vasis.Erp.Facil.Shared.Dtos.Common;
 
-namespace Vasis.Erp.Facil.Data.Repositories.Interfaces.Base
+namespace Vasis.Erp.Facil.Data.Repositories.Interfaces
 {
     public interface IBaseRepository<T> where T : class
     {
-        Task<List<T>> GetAllAsync();
         Task<T?> GetByIdAsync(Guid id);
-        Task<List<T>> FindAsync(Expression<Func<T, bool>> predicate);
         Task<T> AddAsync(T entity);
         Task<T> UpdateAsync(T entity);
         Task DeleteAsync(Guid id);
-        Task<bool> ExistsAsync(Guid id);
-        Task<PagedResultDto<T>> GetPagedAsync(
-            Expression<Func<T, bool>> predicate,
-            PagedRequestDto request
-        );
+        Task<List<T>> GetAllAsync();
+        Task<PagedResult<T>> GetPagedAsync(PagedRequestDto request, CancellationToken cancellationToken = default);
     }
 }
